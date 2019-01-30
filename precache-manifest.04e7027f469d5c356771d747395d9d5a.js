bedc023b9afc86a67615edd1d636596b010e1668
Tanflare <tanflare@gmail.com>
2019-01-30 16:19:42 -0500

    Updates

---
+++ precache-manifest.04e7027f469d5c356771d747395d9d5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6562462929fae4888290",
+    "url": "/ts-website/static/js/runtime~main.65624629.js"
+  },
+  {
+    "revision": "b4311b812089103fedff",
+    "url": "/ts-website/static/js/main.b4311b81.chunk.js"
+  },
+  {
+    "revision": "dde2f9830b75fbaa173a",
+    "url": "/ts-website/static/js/1.dde2f983.chunk.js"
+  },
+  {
+    "revision": "b4311b812089103fedff",
+    "url": "/ts-website/static/css/main.480a71fe.chunk.css"
+  },
+  {
+    "revision": "dde2f9830b75fbaa173a",
+    "url": "/ts-website/static/css/1.2326848a.chunk.css"
+  },
+  {
+    "revision": "d03b13cbe9be859a5636a50fe9602e4f",
+    "url": "/ts-website/index.html"
+  }
+];
