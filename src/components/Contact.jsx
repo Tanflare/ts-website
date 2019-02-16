b91e3c3729f6b3f3b8c817ad5193137fc175c625
Tanflare <tanflare@gmail.com>
2019-02-15 19:31:54 -0500

    Added work and contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,24 @@ class Contact extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className="contact">
-            <h1>Contact</h1>
-            </div>
+            <div className="App">
+                <header className="container">
+                    {/* <h1 className="text">Under Construction</h1> */}
+                    <div class="grid-container">
+                        <div class="grid-item"><img src="../LN.png" height="70px" onClick={linkedin}></img></div>
+                        <div class="grid-item">2</div>
+                        <div class="grid-item">3</div>  
+                    </div>
+                </header>
+            </div> 
          );
     }
 }
  
+function linkedin(){
+var win = window.open("https://www.linkedin.com/in/tanush-samson/", '_blank');
+  win.focus();
+}
+
 export default Contact;
+
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,12 +6,12 @@ class Work extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className="Work">
-            <div className="container">
-            <h1><a href="https://www.behance.net/Tanflare" className="link">Behance</a></h1>
-            <h1><a href="www.behance.com/Tanflare" className="link">GitHub (FIX LINK)</a></h1>
-            <h1><a href="www.behance.com/Tanflare" className="link">MEDIUM (Also fix this)</a></h1>
-            </div>
+            <div className="App">
+                <header className="container">
+                    <h1><a href="https://www.behance.net/Tanflare" className="link">Behance</a></h1>
+                    <br></br>
+                    <h1><a href="https://github.com/tanflare" className="link">GitHub</a></h1>
+                </header>
             </div> 
             
         );
