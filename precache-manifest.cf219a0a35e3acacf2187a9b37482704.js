a6f58b62c2b5de20ec325d4c520ad007921ead55
Tanflare <tanflare@gmail.com>
2019-02-25 21:01:13 -0500

    Updates

--- precache-manifest.cf219a0a35e3acacf2187a9b37482704.js
+++ precache-manifest.cf219a0a35e3acacf2187a9b37482704.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/ts-website/static/js/runtime~main.65624629.js"
   },
   {
-    "revision": "aa6a12355957b28d3e22",
-    "url": "/ts-website/static/js/main.aa6a1235.chunk.js"
+    "revision": "0a4db4bf12dc6bf05687",
+    "url": "/ts-website/static/js/main.0a4db4bf.chunk.js"
   },
   {
     "revision": "dde2f9830b75fbaa173a",
     "url": "/ts-website/static/js/1.dde2f983.chunk.js"
   },
   {
-    "revision": "aa6a12355957b28d3e22",
-    "url": "/ts-website/static/css/main.3cf11202.chunk.css"
+    "revision": "0a4db4bf12dc6bf05687",
+    "url": "/ts-website/static/css/main.5b4f89c8.chunk.css"
   },
   {
     "revision": "dde2f9830b75fbaa173a",
     "url": "/ts-website/static/css/1.2326848a.chunk.css"
   },
   {
-    "revision": "65581a1a3d3e5217ce52f0990c1827f1",
+    "revision": "608a710d71962ce64953f107302427b1",
     "url": "/ts-website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ts-website/precache-manifest.b0cf8927ab7ec5835e885ca7cd1e2f5c.js"
+  "/ts-website/precache-manifest.cf219a0a35e3acacf2187a9b37482704.js"
 );
 
 workbox.clientsClaim();
